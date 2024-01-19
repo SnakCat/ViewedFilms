14489c461227c33ae474f400d7fb1214dc1bf7e6
SnakCat <snakkdss@gmail.com>
2024-01-19 11:59:48 +0300

    create project

---
+++ ViewedFilms/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ViewedFilms
+//
+//  Created by Дмитрий Трушин on 19.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
